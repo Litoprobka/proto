5b8e43a64d1ccf1b94aecc246bda4191e0b69fb9
Peter Burtsev <52280455+Litoprobka@users.noreply.github.com>
2025-05-15 13:24:15 +0300

    drop relude dependency, add a couple of functions

--- src/Json.hs
+++ src/Json.hs
@@ -1,11 +1,14 @@
+-- an example json parser
 module Json where
 
-import Relude hiding (bool, null)
-import Relude.Unsafe (read)
+import Prelude hiding (null)
 import Proto
 import Control.Monad.Combinators
 import Data.Text qualified as Text
+import Data.Text (Text)
 import Data.HashMap.Strict qualified as HashMap
+import Data.HashMap.Strict (HashMap)
+import Data.Maybe (fromMaybe)
 
 data Json
     = Object (HashMap Text Json)
@@ -16,32 +19,34 @@ data Json
     | Null
   deriving (Show, Eq)
 
-lexeme :: Proto a -> Proto a
+type Parser = Proto ()
+
+lexeme :: Parser a -> Parser a
 lexeme p = p <* space
 
-symbol :: Text -> Proto Text
+symbol :: Text -> Parser Text
 symbol = lexeme . string
 
-json, object, array, number, null :: Proto Json
+json, object, array, number, null :: Parser Json
 json = lexeme $ choice [object, array, String <$> string', number, bool, null]
 
 object = Object . HashMap.fromList <$> between (symbol "{") (symbol "}") (sepEndBy keyValue (symbol ","))
 
-keyValue :: Proto (Text, Json)
+keyValue :: Parser (Text, Json)
 keyValue = (,) <$> string' <* symbol ":" <*> json
 
 array = Array <$> between (symbol "[") (symbol "]") (sepEndBy json (symbol ","))
 
-string' :: Proto Text
+string' :: Parser Text
 string' = lexeme $ between (char '"') (symbol "\"") $ takeWhileP (/='"')
 
 number = lexeme $ Number . read . Text.unpack . Text.concat <$> sequence
     [nonZero, digits, fromMaybe "" <$> optional (liftA2 Text.cons (char '.') digits)]
   where
-    nonZero = one <$> satisfy (`elem` ['1'..'9'])
+    nonZero = Text.singleton <$> satisfy (`elem` ['1'..'9'])
     digits = takeWhileP (`elem` ['0'..'9'])
 
-bool :: Proto Json
+bool :: Parser Json
 bool = Bool <$> (True <$ symbol "true" <|> False <$ symbol "false")
 
 null = Null <$ symbol "null"
--- src/Proto.hs
+++ src/Proto.hs
@@ -1,8 +1,8 @@
 module Proto (
     Proto,
-    ErrorPlaceholder (..),
-    skip,
+    fail,
     try,
+    expect,
     takeWhileP,
     takeWhile1P,
     takeP,
@@ -15,107 +15,131 @@ module Proto (
     space,
     space1,
     parse,
+    error,
 ) where
 
 import Data.Char (isSpace)
 import Data.Text qualified as Text
-import Relude
+import Data.Text (Text)
+import Prelude hiding (fail, error)
+import Control.Monad hiding (fail)
+import Control.Applicative
 
 -- monad transformer is TBD
-newtype Proto a = Proto (Text -> ParseStep a) deriving (Functor)
+newtype Proto err a = Proto (Text -> ParseStep err a) deriving (Functor)
 
-newtype ErrorPlaceholder = Err Text deriving (Show)
-
-data ParseStep a
+data ParseStep err a
     = Parsed Text a
-    | Skipped ErrorPlaceholder
-    | Error ErrorPlaceholder
+    | Skipped (Either Text err) -- an expected parse failure
+    | Error (Either Text err)
     deriving (Functor)
 
-instance Applicative Proto where
+instance Applicative (Proto err) where
     pure x = Proto (`Parsed` x)
     Proto wf <*> Proto wx = Proto \s -> case wf s of
         Parsed s' f -> f <$> wx s'
         Skipped err -> Skipped err
         Error err -> Error err
 
-instance Monad Proto where
+instance Monad (Proto err) where
     Proto wx >>= f = Proto \s -> case wx s of
         Parsed s' x -> let Proto wy = f x in wy s'
         Skipped err -> Skipped err
         Error err -> Error err
 
-instance Alternative Proto where
-    empty = Proto \_ -> Skipped (Err "Alternative.empty")
+instance Alternative (Proto err) where
+    empty = Proto \_ -> Skipped (Left "Alternative.empty")
     Proto wx <|> Proto wy = Proto \s -> case wx s of
         Skipped _ -> wy s
         other -> other
 
-instance MonadPlus Proto where
+instance MonadPlus (Proto err) where
     mzero = empty
     mplus = (<|>)
 
 -- | a placeholder for proper errors
-skip :: ParseStep a
-skip = Skipped (Err "error placeholder")
+skip :: ParseStep err a
+skip = Skipped (Left "error placeholder")
 
-try :: Proto a -> Proto a
+-- | signal a backtrackable parse failure
+fail :: Text -> Proto err a
+fail msg = Proto (const $ Skipped $ Left msg)
+
+-- | throw a non-backtrackable parse failure
+error :: Text -> Proto err a
+error msg = Proto (const $ Error $ Left msg)
+
+try :: Proto err a -> Proto err a
 try (Proto parser) = Proto \s -> case parser s of
     Error err -> Skipped err
     other -> other
 
-takeWhileP :: (Char -> Bool) -> Proto Text
+-- | treat parse failure as an error, i.e. expect a parser to always succeed
+-- 
+-- > >>> parser = char 'a' <|> char 'b'
+-- > >>> parse parser "b"
+-- > Right 'b'
+-- >
+-- > >>> parser2 = expect (char 'a') <|> char 'b'
+-- > >>> parse parser2 "b"
+-- > Left (Left "placeholder")
+expect :: Proto err a -> Proto err a
+expect (Proto parser) = Proto \s -> case parser s of
+    Skipped err -> Error err
+    other -> other
+
+takeWhileP :: (Char -> Bool) -> Proto err Text
 takeWhileP p = Proto \s ->
     let (taken, stream) = Text.span p s
      in Parsed stream taken
 
-takeWhile1P :: (Char -> Bool) -> Proto Text
+takeWhile1P :: (Char -> Bool) -> Proto err Text
 takeWhile1P p = Proto \s -> case Text.span p s of
     (taken, s')
         | Text.null taken -> skip
         | otherwise -> Parsed s' taken
 
-takeP :: Int -> Proto Text
+takeP :: Int -> Proto err Text
 takeP n = Proto \s -> case Text.splitAt n s of
     (taken, s')
         | Text.length taken == n -> Parsed s' taken
         | otherwise -> skip
 
-satisfy :: (Char -> Bool) -> Proto Char
+satisfy :: (Char -> Bool) -> Proto err Char
 satisfy p = Proto \s -> case Text.uncons s of
     Just (c, s') | p c -> Parsed s' c
     _ -> skip
 
-eof :: Proto ()
+eof :: Proto err ()
 eof = Proto \s ->
     if Text.null s
         then Parsed s ()
         else skip
 
-notFollowedBy :: Proto a -> Proto ()
+notFollowedBy :: Proto err a -> Proto err ()
 notFollowedBy (Proto notNext) =
     Proto \s -> case notNext s of
-        Parsed _ _ -> Error (Err "placeholder")
+        Parsed _ _ -> Error (Left "placeholder")
         _ -> Parsed s ()
 
-char :: Char -> Proto Char
+char :: Char -> Proto err Char
 char c = satisfy (== c)
 
-anyChar :: Proto Char
+anyChar :: Proto err Char
 anyChar = satisfy (const True)
 
-string :: Text -> Proto Text
+string :: Text -> Proto err Text
 string txt = do
     chunk <- takeP (Text.length txt)
     if chunk == txt
         then pure chunk
-        else Proto (const skip) -- should this be backtrackable?
+        else fail "placeholder"
 
-space, space1 :: Proto ()
+space, space1 :: Proto err ()
 space = void $ takeWhileP isSpace
 space1 = void $ takeWhile1P isSpace
 
-parse :: Proto a -> Text -> Either ErrorPlaceholder a
+parse :: Proto err a -> Text -> Either (Either Text err) a
 parse (Proto parser) input = case parser input of
     Parsed _ output -> Right output
     Skipped err -> Left err
